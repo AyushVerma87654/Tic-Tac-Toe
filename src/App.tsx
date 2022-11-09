1723b98f5a9ded2f5437a74d5facf280dbc69c96
Ayush Verma <tgmcodeyogi@gmail.com>
2022-11-09 14:32:45 +0530

    Heading tag replaced by div tag for bug solving

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import React from "react";
 import Last from "./Last";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./MainPage";
-import Heading from "./Heading";
+// import Heading from "./Heading";
 
 const App = () => {
   return (
     <div>
-      <Heading className="bg-black m-0 fixed top-0 h-20 w-full">
+      <div className="bg-black text-xl flex justify-center items-center text-rose-500 font-bold fixed top-0 h-20 w-full">
         Welcome to my Tic Tac Toe Game...
-      </Heading>
+      </div>
       <Routes>
         <Route index element={<Last />} />
         <Route path="/game" element={<MainPage />} />
       </Routes>
-      <Heading className="bg-black m-0 fixed bottom-0 h-20 w-full">
+      <div className="bg-black text-xl flex justify-center items-center text-rose-500 font-bold fixed bottom-0 h-20 w-full">
         Created & Powered By Ayush
-      </Heading>
+      </div>
     </div>
   );
 };
